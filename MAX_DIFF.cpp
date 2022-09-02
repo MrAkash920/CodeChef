50ead819d5f386ff2c3e0d7aa6024db4f966e89f
Akash Singh <112510755+MrAkash920@users.noreply.github.com>
2022-09-02 15:08:13 +0530

    Add files via upload
    Chef prepared two dishes yesterday. Chef had assigned the \textbf{tastiness}tastiness T_1T 
    1
    ​
      and T_2T 
    2
    ​
      to the first and to the second dish respectively. The tastiness of the dishes can be any \textbf{integer}integer between 00 and NN (both inclusive).
    
    Unfortunately, Chef has forgotten the values of T_1T 
    1
    ​
      and T_2T 
    2
    ​
      that he had assigned to the dishes. However, he remembers the sum of the tastiness of both the dishes - denoted by SS.
    
    Chef wonders, what can be the maximum possible absolute difference between the tastiness of the two dishes. Can you help the Chef in finding the maximum absolute difference?
    
    It is guaranteed that at least one pair \{T_1, T_2\}{T 
    1
    ​
     ,T 
    2
    ​
     } exist such that T_1 + T_2 = S, 0 \leq T_1, T_2 \leq NT 
    1
    ​
     +T 
    2
    ​
     =S,0≤T 
    1
    ​
     ,T 
    2
    ​
     ≤N.
    
    Input Format
    The first line of input contains a single integer TT, denoting the number of testcases. The description of the TT testcases follows.
    The first and only line of each test case contains two space-separated integers NN, SS, denoting the maximum tastiness and the sum of tastiness of the two dishes, respectively.
    Output Format
    For each testcase, output a single line containing the maximum absolute difference between the tastiness of the two dishes.
    
    Constraints
    1 \leq T \leq 10^31≤T≤10 
    3
     
    1 \leq N \leq 10^51≤N≤10 
    5
     
    1 \leq S \leq 2 \cdot 10^51≤S≤2⋅10 
    5

---
+++ MAX_DIFF.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,s;
+	    cin>>n>>s;
+	    int ans=0;
+	    if(s<=n){
+	        ans=s;
+	    }
+	    else{
+	        ans=(n-s)+n;
+	    }
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
